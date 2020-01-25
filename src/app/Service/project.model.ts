6236c5f188724d388230a1cd21af4efc58e29aba
Martha Plantz <maplantz89@gmail.com>
2020-01-24 16:06:45 -0800

    updated projects and added logo

--- src/app/Service/project.model.ts
+++ src/app/Service/project.model.ts
@@ -1,12 +1,10 @@
-import { Image } from './image.model';
-
 export class Project{
   public name: string; 
   public stack: string;
   public description: string; 
-  public imagePaths: Image[]; 
+  public imagePaths: string[]; 
 
-  constructor(name: string, stack: string, description: string, imagePaths: Image[]){
+  constructor(name: string, stack: string, description: string, imagePaths: string[]){
     this.name = name;
     this.stack = stack;
     this.description = description;
