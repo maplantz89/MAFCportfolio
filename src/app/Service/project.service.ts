73c472f7c08efcb7ebc1022573ddc85c7387f66a
Martha Plantz <maplantz89@gmail.com>
2020-01-27 12:03:26 -0800

    revised home content and project descriptions

--- src/app/Service/project.service.ts
+++ src/app/Service/project.service.ts
@@ -5,9 +5,9 @@ export class ProjectService {
   private project: Project[] = [
     new Project(
       'Send It',
-      'Angular, Bootstrap, HTML, Sass',
-      'Coming Soon - Passion project - Frontend Development. Send It is a new fresh way to track your bouldering progress and log all your climbs...',
-      [ 
+      'Frontend Project - Angular, Bootstrap, HTML, Sass',
+      '(Work In Progress) SendIt is a new fresh way to track your bouldering progress and log all your climbs. My climbing and life partner, Aaron Fields-Cypress did the design work while I am responsible for the frontend development.',
+      [
         '../../assets/Images/SendIt/SendIt-Login.png',
         '../../assets/Images/SendIt/SendIt-Loading.png',
         '../../assets/Images/SendIt/SendIt-Climbs.png',
@@ -15,37 +15,47 @@ export class ProjectService {
       ],
       false,
       'https://sprout-app.now.sh/'
-      ),
+    ),
     new Project(
       'Tutor Center',
-      'Adobe XD, Tree Testing, HTML, CSS, Bootstrap',
-      'Coming Soon - Redesign Case Study. Is this just because this description is too short for it to take up the same amount of space?',
+      'Redesign Case Study - Adobe XD, Tree Testing, HTML, CSS, Bootstrap',
+      '(Coming Soon) Tutor Center is a redesign case study centered around user research and user experience.',
       [
         '../../assets/Images/TutorCenter/TutorCenterArch.png'
       ],
       true,
       'https://sprout-app.now.sh/'
-      ),
+    ),
     new Project(
       'Sprout',
-      'React, Node.js, PostgreSQL',
-      'Full-stack Appliction - Sprout is a formative assessment tool used to help teachers deliver specific, goal-oriented, and timely feedback in the classroom.',
+      'Full-stack Appliction - React, Node.js, PostgreSQL',
+      'Sprout is a formative assessment tool used to help teachers deliver specific, goal-oriented, and timely feedback in the classroom. My primary contributions include the student dashboard page as well as backend testing.',
       [
         '../../assets/Images/Sprout/SproutInAction2.png'
       ],
       false,
       'https://sprout-app.now.sh/'
-      ),
+    ),
     new Project(
       'Spaced Repetition',
-      'React, Node.js, PostgreSQL',
-      'Spaced Repetition is a responsive web application that allows users to learn a foreign language, by utilizes an algorithm to implement allowing for a more effective learning experience.',
+      'Full-stack Appliction - React, Node.js, PostgreSQL',
+      'Spaced Repetition is a responsive web application that allows users to learn a foreign language, by utilizes an algorithm to implement allowing for a more effective learning experience. My primary contributions including styling/design and collaborating with team members around the algorithm in the server.',
       [
         '../../assets/Images/SpacedRepetition/dashboard.png'
       ],
       false,
       'https://spaced-repetition.mac-thinkful.now.sh/register/'
-      ),
+    ),
+    new Project(
+      'myCookbook',
+      'Full-stack Appliction - React, Node.js, PostgreSQL',
+      'myCookbook built entirely, idea to deployment in two weeks. MyCookbook is intended to allows users to search, create and organize recipes into multiple cookbooks.',
+      [
+        '../../assets/Images/myCookbook/cookbookApp.png'
+      ],
+      false,
+      'https://my-cookbook-app.maplantz89.now.sh/login'
+    )
   ];
 
   getProjects() {
